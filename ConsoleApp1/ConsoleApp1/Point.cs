072e66053fd39e0d9b4dec610425aff085066976
esterkamionka <102261639+esterkamionka@users.noreply.github.com>
2022-06-19 00:27:20 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Point
+    {
+        private int row;
+        private int col;
+
+        public int Row { get { return row; } set { row = value; } }
+        public int Col { get { return col; } set { col = value; } }
+        public Point(int r, int c)
+        {
+            row = r;
+            col = c;
+        }
+    }
+
+
+}
+
---
+++ ConsoleApp1/ConsoleApp1/SubMarine1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class SubMarine1 : SubMarine
+    {
+        public SubMarine1()
+        {
+            length = 1;
+            Type = Type.subMarine1;
+        }
+    }
+    class Submarine2 : SubMarine
+    {
+        public Submarine2()
+        {
+            length = 2;
+            Type = Type.subMarine2;
+        }
+    }
+    class Submarine3 : SubMarine
+    {
+        public Submarine3()
+        {
+            length = 3;
+            Type = Type.subMarine3;
+        }
+
+    }
+    class Submarine4 : SubMarine
+    {
+        public Submarine4()
+        {
+            length = 4;
+            Type = Type.subMarine4;
+        }
+    }
+}
