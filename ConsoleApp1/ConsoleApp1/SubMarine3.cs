1d9fb23e4d3d7b083a012d340922becbb27257bb
esterkamionka <102261639+esterkamionka@users.noreply.github.com>
2022-06-19 03:47:19 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/SubMarine3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Submarine3 : SubMarine
+    {
+        public Submarine3()
+        {
+            length = 3;
+            Type = Type.subMarine3;
+            SubmarineCells = new List<SubmarineCell>();
+        }
+        public override void Build_Vertical(SubmarineCell cell)
+        {
+            for (int i = cell.Row; i < cell.Row + length; i++)
+                SubmarineCells.Add(new SubmarineCell(i, cell.Col, Type));
+
+        }
+        public override void Build_Horizontal(SubmarineCell cell)
+        {
+            for (int i = cell.Col; i < cell.Col + length; i++)
+                SubmarineCells.Add(new SubmarineCell(cell.Row, i, Type));
+
+        }
+    }
+}
